53f8b51bf885b9e1e791598037021c3351c49495
yury-stepanov <advers191@gmail.com>
2018-04-23 22:27:50 +0300

    Removed custom fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'typeface-roboto';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
